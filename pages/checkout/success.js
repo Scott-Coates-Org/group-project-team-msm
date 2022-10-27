7b37d6210b97adc274a8645c57fe7462ccc6b47e
Aubrey <aubreyfinkelstein@gmail.com>
2022-10-26 21:50:02 -0700

    minor corrections to comments on the pages/checkout/success page

--- pages/checkout/success.js
+++ pages/checkout/success.js
@@ -5,13 +5,13 @@ import Button from 'react-bootstrap/Button';
 import { ProtectedRoute } from '../../components/protectedPage';
 
 //TO-DO: should only be reachable when customer is redirected here from backend, so need some sort of global state/parameter to confirm that
-//WATCH: this is set up using the ProjectedRoute component, if method of showing a user is logged in changes this will need to be re-worked
+//WATCH: this is set up using the ProtectedRoute component, if method of showing a user is logged in changes this will need to be re-worked
 //TO-DO: once we know the Layout that will be used on all pages, get this to vertically center
     //(need to make parent container a flex and then use it to center this)
 
 // inputs: none
 // returns: JSX component with a thank you message and a return to home button
-// This page/route will be provided to stripe for returning to our app after a successful payment
+// This page/route will be redirected to by the backend after a successful payment event
 function CheckoutSuccess(){
     return (
         <ProtectedRoute>
